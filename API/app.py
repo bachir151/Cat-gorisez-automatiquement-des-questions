4e2ec5170f236f9c4561f9b3f4b1dc28d1eebc89
bachir151 <elbachir90@gmail.com>
2023-07-30 20:09:11 +0200

    Ajout de comm

--- API/app.py
+++ API/app.py
@@ -2,27 +2,37 @@ import numpy as np
 from flask import Flask, request, jsonify, render_template
 from modules import preprocessing, predict_tags, final_preprocessing
 
-# Create flask app
+# Création de l'application Flask
 flask_app = Flask(__name__)
 
+# endpoints pour la page d'accueil
 @flask_app.route("/")
 def Home():
     return render_template("index.html")
 
-@flask_app.route("/predict/", methods = ["POST"])
+# endpoints pour la prédiction
+@flask_app.route("/predict/", methods=["POST"])
 def predict():
+    # Récupération du texte saisi par l'utilisateur dans les champs "title" et "body" du formulaire
     title = request.form['title']
     body = request.form['body']
-    X= title + body
+
+    # Concaténation du titre et du corps pour former le texte complet à prédire
+    X = title + body
+
+    # Prétraitement du texte : suppression des caractères spéciaux, mise en minuscules, etc.
     doc = preprocessing(X, rejoin=True)
-    doc =final_preprocessing(doc)
+    doc = final_preprocessing(doc)
+
+    # Prédiction des tags pour le texte donné
     prediction = predict_tags(doc)
 
-    # Inverse multilabel binarizer
-    #tags_predict = multilabel_binarizer.inverse_transform(prediction)
-    return render_template("index.html", prediction_text = "Tag(s) prédit(s) : {}".format(prediction))
+    # Renvoi des résultats de prédiction au template "index.html" pour affichage à l'utilisateur
+    return render_template("index.html", prediction_text="Tag(s) prédit(s) : {}".format(prediction))
 
+# Démarrage de l'application Flask
 if __name__ == "__main__":
     flask_app.run(debug=True)
+
 
 
